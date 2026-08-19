64462e6b03f80a8637f8a4cb56d09663bd85213a
Kam41514 <slmbnekamil@gmail.com>
2026-08-19 19:08:44 +0300

    Update Icon.lua

--- Icon.lua
+++ Icon.lua
@@ -1,1 +1,20 @@
+--// MoonHub Custom Icons
 
+local Icons = {}
+
+Icons.Moon = {
+    AssetId = 4512627654,
+    Name = "Moon"
+}
+
+function Icons:Get(Name)
+    local Icon = self[Name]
+
+    if not Icon then
+        return nil
+    end
+
+    return Icon.AssetId
+end
+
+return Icons
